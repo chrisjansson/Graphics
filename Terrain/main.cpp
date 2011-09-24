5159a0242c0d6bd5c98c653eef2bbc9509108fe3
Chris Jansson <chris.janssom@gmail.com>
2011-09-24 21:43:07 +0200

    Added noise class and render a cube in OpenGL

--- Terrain/main.cpp
+++ Terrain/main.cpp
@@ -1,7 +1,103 @@
 #include <iostream>
+#include <SFML/window.hpp>
+
+void ReSize(int width, int height) 
+{
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective (50.0, (float)width/(float)height, 1.f, 500.f);
+}
+
+void Render() 
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Apply some transformations
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.f, 0.f, -200.f);
+
+	// Draw a cube
+	glBegin(GL_QUADS);
+	{
+		glVertex3f(-50.f, -50.f, -50.f);
+		glVertex3f(-50.f,  50.f, -50.f);
+		glVertex3f( 50.f,  50.f, -50.f);
+		glVertex3f( 50.f, -50.f, -50.f);
+
+		glVertex3f(-50.f, -50.f, 50.f);
+		glVertex3f(-50.f,  50.f, 50.f);
+		glVertex3f( 50.f,  50.f, 50.f);
+		glVertex3f( 50.f, -50.f, 50.f);
+
+		glVertex3f(-50.f, -50.f, -50.f);
+		glVertex3f(-50.f,  50.f, -50.f);
+		glVertex3f(-50.f,  50.f,  50.f);
+		glVertex3f(-50.f, -50.f,  50.f);
+
+		glVertex3f(50.f, -50.f, -50.f);
+		glVertex3f(50.f,  50.f, -50.f);
+		glVertex3f(50.f,  50.f,  50.f);
+		glVertex3f(50.f, -50.f,  50.f);
+
+		glVertex3f(-50.f, -50.f,  50.f);
+		glVertex3f(-50.f, -50.f, -50.f);
+		glVertex3f( 50.f, -50.f, -50.f);
+		glVertex3f( 50.f, -50.f,  50.f);
+
+		glVertex3f(-50.f, 50.f,  50.f);
+		glVertex3f(-50.f, 50.f, -50.f);
+		glVertex3f( 50.f, 50.f, -50.f);
+		glVertex3f( 50.f, 50.f,  50.f);
+	}
+	glEnd();
+}
+
+void Init()
+{
+	// Set color and depth clear value
+    glClearDepth(1.f);
+    glClearColor(0.f, 0.f, 0.f, 0.f);
+
+    // Enable Z-buffer read and write
+    glEnable(GL_DEPTH_TEST);
+    glDepthMask(GL_TRUE);
+
+	ReSize(800, 600);
+}
 
 int main(int argc, char** argv) 
 {
-	std::cout << "Hello World!" << std::endl;
+	sf::WindowSettings settings;
+	settings.DepthBits         = 24; // Request a 24 bits depth buffer
+	settings.StencilBits       = 8;  // Request a 8 bits stencil buffer
+	settings.AntialiasingLevel = 2;  // Request 2 levels of antialiasing
+	sf::Window app(sf::VideoMode(800, 600, 32), "SFML OpenGL", sf::Style::Resize, settings);
+	
+	Init();
+
+	while(app.IsOpened()) 
+	{
+		sf::Event event;
+		while(app.GetEvent(event)) 
+		{
+            // Close window : exit
+            if (event.Type == sf::Event::Closed)
+                app.Close();
+
+            // Escape key : exit
+            if ((event.Type == sf::Event::KeyPressed) && (event.Key.Code == sf::Key::Escape))
+                app.Close();
+
+			if (event.Type == sf::Event::Resized)
+				ReSize(event.Size.Width, event.Size.Height);
+		}
+		app.SetActive();
+
+		Render();
+
+		app.Display();
+	}
+
 	return 0;
 }
